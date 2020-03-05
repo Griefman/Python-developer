801db5f42d0c6ec0ba448190649ad90680cfadf1
Griefman <stanislavparshin@yandex.ru>
2020-03-05 22:55:21 +0300

    add two tests for functions prime_num

--- Lesson-05/divisor_master.py
+++ Lesson-05/divisor_master.py
@@ -31,3 +31,37 @@ def get_max_prime(n):
             lst_max_prime.append(i)
     return max(lst_max_prime)
 
+# Тест №1 - Проверка числа на простоту (истинные значения)
+
+
+def test_1():
+    dct_in = {'7': True, '101': True, '997': True}
+    dct_out = {}
+    for key in dct_in.keys():
+        bool_out = prime_num(int(key))
+        dct_out[key] = bool_out
+    if dct_in == dct_out:
+        print('Test #1 OK')
+    else:
+        print('Test #1 is failed')
+
+
+# Тест №2 - Проверка числа на простоту (ложные значения)
+
+
+def test_2():
+    dct_in = {'4': False, '100': False, '500': False}
+    dct_out = {}
+    for key in dct_in.keys():
+        bool_out = prime_num(int(key))
+        dct_out[key] = bool_out
+    if dct_in == dct_out:
+        print('Test #2 OK')
+    else:
+        print('Test #2 is failed')
+
+
+test_1()
+test_2()
+
+
