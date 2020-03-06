96b98f990ab60955e144382174d9f807a55195ff
Griefman <stanislavparshin@yandex.ru>
2020-03-06 13:28:11 +0300

    rewrite tests with unittest

--- Lesson-06/tests.py
+++ Lesson-06/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Проверка числа на простоту
+import unittest
 
 
 def prime_num(n):
@@ -31,84 +32,120 @@ def get_max_prime(n):
             lst_max_prime.append(i)
     return max(lst_max_prime)
 
-# Тест №1 - Проверка числа на простоту (истинные значения)
+
+class TestPrimeNum(unittest.TestCase):
+    def test_simple_1(self):
+        self.assertTrue(prime_num(101))
+
+    def test_simple_2(self):
+        self.assertFalse(prime_num(102))
 
 
-def test_1():
-    dct_in = {'7': True, '101': True, '997': True}
-    dct_out = {}
-    for key in dct_in.keys():
-        bool_out = prime_num(int(key))
-        dct_out[key] = bool_out
-    if dct_in == dct_out:
-        print('Test #1 OK')
-    else:
-        print('Test #1 is failed')
+class TestGetDivisors(unittest.TestCase):
+    def test_simple_3(self):
+        self.assertEqual(get_divisors(50), [1, 2, 5, 10, 25, 50])
 
 
-# Тест №2 - Проверка числа на простоту (ложные значения)
+class TestGetMaxPrime(unittest.TestCase):
+    def test_simple_1(self):
+        self.assertEqual(get_max_prime(37), 37)
+
+    def test_simple_2(self):
+        self.assertNotEqual(get_max_prime(997), 996)
 
 
-def test_2():
-    dct_in = {'4': False, '100': False, '500': False}
-    dct_out = {}
-    for key in dct_in.keys():
-        bool_out = prime_num(int(key))
-        dct_out[key] = bool_out
-    if dct_in == dct_out:
-        print('Test #2 OK')
-    else:
-        print('Test #2 is failed')
 
 
-# Тест №3 - Проверка делителей числа
 
 
-def test_3():
-    dct_in = {'4': [1, 2, 4], '50': [1, 2, 5, 10, 25, 50], '100': [1, 2, 4, 5, 10, 20, 25, 50, 100]}
-    dct_out = {}
-    for key in dct_in.keys():
-        lst_out = get_divisors(int(key))
-        dct_out[key] = lst_out
-    if dct_in == dct_out:
-        print('Test #3 OK')
-    else:
-        print('Test #3 is failed')
-
-# Тест №4 - Проверка максимального делителя числа (Истинные значения)
 
 
-def test_4():
-    dct_in = {'100': 5, '666': 37, '2800': 7}
-    dct_out = {}
-    for key in dct_in.keys():
-        num_out = get_max_prime(int(key))
-        dct_out[key] = num_out
-    if dct_in == dct_out:
-        print('Test #4 OK')
-    else:
-        print('Test #4 is failed')
 
 
-# Тест №5 - Проверка максимального делителя числа, используя
-#  натуральные числа, у которых максимальный делитель совпадает с самим числом.
 
 
-def test_5():
-    lst_in = [7, 37, 997]
-    lst_out = []
-    for i in range(len(lst_in)):
-        num_out = get_max_prime(lst_in[i])
-        if num_out == lst_in[i]:
-            lst_out.append(num_out)
-    if lst_in == lst_out:
-        print('Test #5 OK')
-    else:
-        print('Test #5 is failed')
 
 
-test_1()
-test_2()
-test_3()
-test_4()
-test_5()
+
+# # Тест №1 - Проверка числа на простоту (истинные значения)
+#
+#
+# def test_1():
+#     dct_in = {'7': True, '101': True, '997': True}
+#     dct_out = {}
+#     for key in dct_in.keys():
+#         bool_out = prime_num(int(key))
+#         dct_out[key] = bool_out
+#     if dct_in == dct_out:
+#         print('Test #1 OK')
+#     else:
+#         print('Test #1 is failed')
+#
+#
+# # Тест №2 - Проверка числа на простоту (ложные значения)
+#
+#
+# def test_2():
+#     dct_in = {'4': False, '100': False, '500': False}
+#     dct_out = {}
+#     for key in dct_in.keys():
+#         bool_out = prime_num(int(key))
+#         dct_out[key] = bool_out
+#     if dct_in == dct_out:
+#         print('Test #2 OK')
+#     else:
+#         print('Test #2 is failed')
+#
+#
+# # Тест №3 - Проверка делителей числа
+#
+#
+# def test_3():
+#     dct_in = {'4': [1, 2, 4], '50': [1, 2, 5, 10, 25, 50], '100': [1, 2, 4, 5, 10, 20, 25, 50, 100]}
+#     dct_out = {}
+#     for key in dct_in.keys():
+#         lst_out = get_divisors(int(key))
+#         dct_out[key] = lst_out
+#     if dct_in == dct_out:
+#         print('Test #3 OK')
+#     else:
+#         print('Test #3 is failed')
+#
+# # Тест №4 - Проверка максимального делителя числа (Истинные значения)
+#
+#
+# def test_4():
+#     dct_in = {'100': 5, '666': 37, '2800': 7}
+#     dct_out = {}
+#     for key in dct_in.keys():
+#         num_out = get_max_prime(int(key))
+#         dct_out[key] = num_out
+#     if dct_in == dct_out:
+#         print('Test #4 OK')
+#     else:
+#         print('Test #4 is failed')
+#
+#
+# # Тест №5 - Проверка максимального делителя числа, используя
+# #  натуральные числа, у которых максимальный делитель совпадает с самим числом.
+#
+#
+# def test_5():
+#     lst_in = [7, 37, 997]
+#     lst_out = []
+#     for i in range(len(lst_in)):
+#         num_out = get_max_prime(lst_in[i])
+#         if num_out == lst_in[i]:
+#             lst_out.append(num_out)
+#     if lst_in == lst_out:
+#         print('Test #5 OK')
+#     else:
+#         print('Test #5 is failed')
+#
+#
+# test_1()
+# test_2()
+# test_3()
+# test_4()
+# test_5()
+
