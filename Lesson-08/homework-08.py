c31e9628fee101ca1595507e24cc760eb036df1e
Griefman <stanislavparshin@yandex.ru>
2020-04-21 22:53:09 +0300

    fix hw-8

--- Lesson-08/homework-08.py
+++ Lesson-08/homework-08.py
@@ -4,23 +4,34 @@ import time
 
 
 def show_time(f):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         time_start = time.time()
-        f()
+        f(*args, **kwargs)
         time_end = time.time()
+        print('Начало выполнения: ', time_start, 'Завершение выполнения: ', time_end)
         print('Время выполнения: ', time_end - time_start, 'sec')
-
-    return wrapper()
-
-
-@show_time
-def list_generator():
-    return [i for i in range(1, 1000000 + 1)]
+    return wrapper
 
 
 @show_time
-def list_creator():
+def list_creator(num):
     list_numbers = []
-    for i in range(1, 1000000 + 1):
+    for i in range(1, num + 1):
         list_numbers.append(i)
     return list_numbers
+
+
+list_creator(1000000)
+
+
+@show_time
+def list_generator(num):
+    for i in range(1, num + 1):
+        yield i
+
+
+list_generator(1000000)
+
+
+
+
